f5cbd18465aca4f06b9009c8723cc6909e41cbef
TJZjc <1344179523@qq.com>
2024-06-14 23:12:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #f2f4f7;
+  width: 100%;
+  height: 98vh;
+  display: inline-block;
+}
+</style>
